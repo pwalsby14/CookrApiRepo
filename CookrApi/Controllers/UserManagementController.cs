a4b4a56220ac486039d33c1af1d546abdbef854b
pwalsby14 <peterwalsbyconsulting@gmail.com>
2018-04-15 15:00:37 +0100

    Initial Commit

---
+++ CookrApi/Controllers/UserManagementController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CookrApi.Controllers
+{
+    public class UserManagementController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
