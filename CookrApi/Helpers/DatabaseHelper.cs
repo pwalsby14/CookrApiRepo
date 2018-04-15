89f7aaf8c7e2e55fd91c4ef8394d41ca96b20f85
pwalsby14 <peterwalsbyconsulting@gmail.com>
2018-04-15 15:21:15 +0100

    Added new Excel Helper class and nuget packages for sql

---
+++ CookrApi/Helpers/DatabaseHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookrApi.Helpers
+{
+    public class DatabaseHelper
+    {
+        public static SqlConnection ConnectToDatabase()
+        {
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "your_server.database.windows.net";
+                builder.UserID = "your_user";
+                builder.Password = "your_password";
+                builder.InitialCatalog = "your_database";
+
+                SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                
+                return connection;
+                
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+                return null;
+            }
+        }
+    }
+}
